a3e19ccd285958c7e1123bab4d3ae19204f794df
0lanyu0 <2176927988@qq.com>
2026-03-13 23:05:33 +0800

    Mapper

---
+++ CourseMapper.java
@@ -0,0 +1,31 @@
+package com.rabbiter.sms.dao.Course;
+
+import com.github.pagehelper.PageRowBounds;
+import com.rabbiter.sms.dto.Course;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Mapper
+public interface CourseMapper {
+
+  void addCourse(Course course);
+
+
+  void delete(@Param("ids") List<Long> ids);
+
+
+  void update(Course course);
+
+  List<Course> getCourseList(PageRowBounds rowBounds, @Param("condition") Map<String, Object> condition);
+
+  String checkCodeCount(@Param("condition") Map<String, Object> condition);
+
+  List<Course> getCourseByMap(@Param("condition") Map<String, Object> condition);
+
+
+  Course getCourseById(@Param("courseId") String courseId);
+}
